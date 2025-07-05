bc027210d00af5787d7bc0b499915dc2c97d5399
PedroRomeroEracing <pedro.santos@unicamperacing.com.br>
2025-07-05 15:18:49 -0300

    2 tentativa de upload da planilha implementada

--- box_telemetria.py
+++ box_telemetria.py
@@ -13,7 +13,6 @@ planilha_BMS = pd.read_csv(
     header=None, skip_blank_lines=True, comment='/'
 )
 
-
 def on_connect(client, userdata, flags, rc): #conexão com o broker
     print("Conectado ao broker")
     client.subscribe("telemetria")
@@ -36,15 +35,28 @@ def tratamento_mensagem(dados): #dados é um dicionário com as mensagens recebi
     else:
         extrai_planilha(id_hexadecimal, data, planilha_BMS)
 
-def associação_mensagem_planilha(nome, campo_bit, campo_multiplicador, campo_descrição, planilha, data, lista_bytes_bits_invertidos, lista_bytes_bits_invertidos_concatenados):
+def associação_mensagem_planilha(nome, campo_bit, campo_multiplicador, campo_descrição, planilha, data, lista_bytes_bits_invertidos, string_bytes_bits_invertidos_concatenados):
     # associa o bit da planilha para aquela variável com o bit da mensagem recebida
     if campo_bit.startswith('bit('):
         range_bits = campo_bit.replace('bit(', '').replace(')', '').split('-')
-        bit_ini = int(range_bits[0])
-        bit_fim = int(range_bits[-1])
         # 'bit(10-11)' -> bits = ['10', '11']
+        bit_ini = int(range_bits[0]) # primeiro bit
+        bit_fim = int(range_bits[1]) # último bit
+        mensagem = string_bytes_bits_invertidos_concatenados[bit_ini:bit_fim + 1]  # pega os bits da mensagem
+    
+    elif campo_bit.startswith('byte('):
+        bytes_ = campo_bit.replace('byte(', '').replace(')', '').split('-')
+        byte_ini = int(bytes_[0]) #primeiro byte
+        # 'byte(0-1)' -> bytes = ['0', '1']
+        if len(bytes_) == 1:
+            # byte(0) -> [0*8 até 0*8 + 8] bits
+            mensagem = lista_bytes_bits_invertidos[byte_ini*8:byte_ini*8 + 8]  # pega o byte inteiro, está em bits concatenados
+        else:
+            byte_fim = int(bytes_[1]) # último byte
+            mensagem = string_bytes_bits_invertidos_concatenados[byte_ini*8:byte_fim*8 + 8]
 
-
+    mensagem_int = int(mensagem, 2)  # converte de binário para inteiro
+    print(f"Mensagem '{nome}': bits :{mensagem_int*campo_multiplicador}, descrição: {campo_descrição}")
 
 def extrai_planilha(id_hexadecimal, data, planilha):
     #Achar linha do id na planilha e pegar variável - bit
@@ -59,7 +71,7 @@ def extrai_planilha(id_hexadecimal, data, planilha):
         bits_str = bin(byte)[2:].zfill(8)  # ex: '00001000'
         bits_str = bits_str[::-1]          # inverte: '00010000'
         lista_bytes_bits_invertidos.append(bits_str)
-    lista_bytes_bits_invertidos_concatenados = ''.join(lista_bytes_bits_invertidos)  # junta todos os bits LSB primeiro)
+    string_bytes_bits_invertidos_concatenados = ''.join(lista_bytes_bits_invertidos)  # junta todos os bits (LSB primeiro)
 
     # Extrair os dados relevantes da planilha
     for i in range(idx_inicio, idx_fim):
@@ -67,8 +79,7 @@ def extrai_planilha(id_hexadecimal, data, planilha):
         campo_bit = planilha.iloc[i, 2] # coluna 3: especificação (bit(x-y), byte(x), etc)
         campo_multiplicador = planilha.iloc[i, 5]  # coluna 5: multiplicador
         campo_descrição = planilha.iloc[i, 9]   # coluna 9: descrição
-        associação_mensagem_planilha(nome, campo_bit, campo_multiplicador, campo_descrição, planilha, data, lista_bytes_bits_invertidos, lista_bytes_bits_invertidos_concatenados)
-
+        associação_mensagem_planilha(nome, campo_bit, campo_multiplicador, campo_descrição, planilha, data, lista_bytes_bits_invertidos, string_bytes_bits_invertidos_concatenados)
 
 client = mqtt.Client()
 client.connect("172.20.10.2", 1883)  #IP do broker, proprio notebook para se escutar
