2f81cb26d7fcef0647258538fca171cadceb93c8
PedroRomeroSantos <galaga100prs@gmail.com>
2025-07-21 22:52:08 -0300

    código da jetson atualizado

--- car_telemetria.py
+++ car_telemetria.py
@@ -1,8 +1,10 @@
 import can
+import struct
 import paho.mqtt.client as mqtt
 import json
+import time
 
-broker_ip = "192.168.0.100"  # IP do box, enviar pra notebook
+broker_ip = "172.20.10.2"  # IP do box, enviar pra notebook
 broker_port = 1883
 mqtt_topic = "telemetria"
 can_interface = "can0"
@@ -12,26 +14,29 @@ mqtt_client = mqtt.Client()
 mqtt_client.connect(broker_ip, broker_port)
 print(f"Conectado ao broker MQTT em {broker_ip}:{broker_port}")
 
-print(f"leitura da interface {can_interface}")
-bus = can.interface.Bus(channel=can_interface, bustype='socketcan')
+print(f"Leitura da interface {can_interface}")
+bus = can.interface.Bus(channel=can_interface, bustype="socketcan")
 
 try:
     while True:
         msg = bus.recv()
-        #para filtrar por ID
-        '''
-        if msg.arbitration_id == 0x18FF1515:
-            dados = {
-            ...
-        '''
         dados = {
-                "arbitration_id": msg.arbitration_id,
-                "data": list(msg.data),
-                "timestamp": msg.timestamp
-            }
+            "arbitration_id": msg.arbitration_id,
+            "data": list(msg.data),
+            "timestamp": msg.timestamp
+        }
 
-        mqtt_client.publish(mqtt_topic, json.dumps(dados)) #manda como string JSON
-        print("Enviado:", dados)
+        # Verificar se é mensagem de 1 byte (ID de requisição)
+        if len(dados["data"]) == 1:
+            print(f"Id de requisicao {hex(dados['arbitration_id'])}")
+            continue
+        else:
+            mqtt_client.publish(mqtt_topic, json.dumps(dados))
+            ids_excluidos = "789"
+            if str(dados["arbitration_id"]) not in ids_excluidos:
+                id_hexa = hex(dados["arbitration_id"])
+                print("Id_Hexa:", id_hexa)
+                print("Enviado:", dados)
 
 except KeyboardInterrupt:
     print("\nParou")
